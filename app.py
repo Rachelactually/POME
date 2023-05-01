a15c6e7fa6f4e09a6a79a6f081aa5b3c2e3402df
Rachelactually <132147791+Rachelactually@users.noreply.github.com>
2023-05-01 21:31:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -253,6 +253,7 @@ def main():
             '''
         st.write(mystyle, unsafe_allow_html=True)
         
+        '''
         import base64
         def add_bg_from_local(image_file):
             with open(image_file, "rb") as image_file:
@@ -269,6 +270,8 @@ def main():
             unsafe_allow_html=True
             )
         add_bg_from_local('Wallpaper.jpg')    
+        
+        '''
     ########################################################################
 if __name__=='__main__':
     main()
