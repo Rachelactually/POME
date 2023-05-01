d54bf65ccc98d4327fcf8e9721555c48f160519a
Rachelactually <132147791+Rachelactually@users.noreply.github.com>
2023-05-01 21:35:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -253,25 +253,7 @@ def main():
             '''
         st.write(mystyle, unsafe_allow_html=True)
         
-        """
-        import base64
-        def add_bg_from_local(image_file):
-            with open(image_file, "rb") as image_file:
-                encoded_string = base64.b64encode(image_file.read())
-            st.markdown(
-            f"""
-            <style>
-            .stApp {{
-                background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-                background-size: cover
-            }}
-            </style>
-            """,
-            unsafe_allow_html=True
-            )
-        add_bg_from_local('Wallpaper.jpg')    
-        
-        """
+
     ########################################################################
 if __name__=='__main__':
     main()
