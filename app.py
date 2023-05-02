51cfa9c99676972905ecebada3b77ed78e2a1412
Rachelactually <132147791+Rachelactually@users.noreply.github.com>
2023-05-02 21:32:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -82,9 +82,9 @@ with col3:
     #displaying the image on streamlit app
     st.image(image)
     
-st.caption('**© 2023 Website is the creation of Qian Yee Ong, Xin Yun Kiew and Joshua Liew Y. L. \
-under the :blue[Department of Chemical with Environmental Engineering], University of Nottingham Malaysia.**')
-st.caption('This app predicts the biogas output from a closed system POME anaerobic digestion process.')
+st.caption('© 2023 Website is the creation of **Q.Y. Ong**, **X.Y. Kiew** and **Joshua Liew Y.L.** \
+under the :blue[**Department of Chemical with Environmental Engineering**], **University of Nottingham Malaysia.**')
+st.write('This app predicts the biogas output from a closed system POME anaerobic digestion process.')
 #Create tabs
 tab1, tab2, tab3, tab4 = st.tabs(["Prediction models", "Methodology", "Sustainability", "About"])
 
