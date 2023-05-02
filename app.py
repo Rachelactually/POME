062e6aae6e43c860c2d5daa310113600cd6a9018
Rachelactually <132147791+Rachelactually@users.noreply.github.com>
2023-05-02 17:40:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -383,6 +383,11 @@ with tab2:
 
         
         st.subheader('Random Forest')
+        h1 = '<p font-size: 5px;"><strong>max_depth<strong>\
+        <em>max no. of levels in each decision tree<em>\
+        <br>[20, 30, 50, 80, 100, None]</p>'
+        st.markdown(h1, unsafe_allow_html=True)
+        
         st.markdown("""
         **max_depth** _max no. of levels in each decision tree_
         [20, 30, 50, 80, 100, None]
