779979a98405b30bd2cfcc4bb0bd94e7b9f9c2b5
Rachelactually <132147791+Rachelactually@users.noreply.github.com>
2023-05-02 14:00:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -111,8 +111,9 @@ def main():
     ########################################################################
     # Create subheaders for main performance indicator 
         
+        st.write('##Markdown')
         new_title = '<p style="color:red; font-size: 30px;">Predicting biogas components</p>'
-        st.markdown(**new_title**, unsafe_allow_html=True)
+        st.markdown(new_title, unsafe_allow_html=True)
         st.caption('The **Gaussian Process Regressor (GPR)** model, **Random Forest (RF)** model and **Extreme Gradient Booosting (XGBoost)** model\
         are among the selected predictors for POME biogas components. The accuracy of the respective models, represented by the :blue[R$^{2}$ coefficient of\
         determination] on the prediction of the target outputs are shown in :blue[_italic_].\
