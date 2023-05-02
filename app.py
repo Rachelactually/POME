bd860b66dea0cffad2c155cc7bb2c6566062109a
Rachelactually <132147791+Rachelactually@users.noreply.github.com>
2023-05-02 13:51:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -117,8 +117,7 @@ def main():
         are among the selected predictors for POME biogas components. The accuracy of the respective models, represented by the :blue[R$^{2}$ coefficient of\
         determination] on the prediction of the target outputs are shown in :blue[_italic_].\
         The predicted components incclude total biogas production, methane (CH$_{4}$),\
-        carbon dioxide (CO$_{2}$) and hydrogen sulphide (H$_{2}$S).
-        ')   
+        carbon dioxide (CO$_{2}$) and hydrogen sulphide (H$_{2}$S).')   
 
         # GPR
         new_title = '<p style="font-family:monospace; color:black; font-size: 20px;">Gaussian Process Regressor (GPR)</p>'
