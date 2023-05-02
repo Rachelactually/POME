aa54c6a2d62e11405a5f852976ccf6c96226f6db
Rachelactually <132147791+Rachelactually@users.noreply.github.com>
2023-05-02 21:53:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
 hide_footer_style = """
-        <style>
-        .reportview-container .main footer {visibility: hidden;}    
+<style>
+.reportview-container .main footer {visibility: hidden;}    
 """
 st.markdown(hide_footer_style, unsafe_allow_html=True)
 
