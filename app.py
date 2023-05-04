bbb0f31092f0acfc0ca732d77d7235438d5e40fb
Rachelactually <132147791+Rachelactually@users.noreply.github.com>
2023-05-04 14:54:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -183,6 +183,31 @@ st.write('**This app predicts the biogas output from a closed system POME anaero
 
 #MAKING A BUTTON
 result = st.button("Click here to try it out!", type="primary")
+st.markdown(""" 
+div.stButton > button:first-child {
+background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+}
+.css-2trqyj:focus:not(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:active){
+background-color: #0066cc;
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+""", unsafe_allow_html=True)
+
 if result:
         
     ##################################################################################################################################################################################
