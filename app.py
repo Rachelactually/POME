600eba68ee5aa6299e893cc7546337521359fb1a
Rachelactually <132147791+Rachelactually@users.noreply.github.com>
2023-05-04 14:55:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import numpy as np
 import pickle
 import streamlit as st
 import altair as alt
+import toml
 from PIL import Image
 ##################################################################################################################################################################################
 
@@ -183,30 +184,13 @@ st.write('**This app predicts the biogas output from a closed system POME anaero
 
 #MAKING A BUTTON
 result = st.button("Click here to try it out!", type="primary")
-st.markdown(""" 
-div.stButton > button:first-child {
-background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
-}
-.css-2trqyj:focus:not(:active) {
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-.css-2trqyj:focus:(:active) {
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-.css-2trqyj:focus:active){
-background-color: #0066cc;
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-""", unsafe_allow_html=True)
+primaryColor = toml.load(".streamlit/config.toml")['theme']['primaryColor']
+s = f"""
+<style>
+div.stButton > button:first-child {{ border: 5px solid {primaryColor}; border-radius:20px 20px 20px 20px; }}
+<style>
+"""
+st.markdown(s, unsafe_allow_html=True)
 
 if result:
         
